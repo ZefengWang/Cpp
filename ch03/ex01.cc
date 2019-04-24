b1854e59c5bb4588cd67a417a0457ee0ea7d31d9
ZefengWang <627132520@qq.com>
2019-04-25 00:21:36 +0800

    ex01.cc

---
+++ ch03/ex01.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	const double factor = 1.0/12;
+	using namespace std;
+	int height;
+	cout << "Please input your height in inch: ___" << "\b\b\b";
+	cin >> height;
+	cout << "Your height in feet is " << height * factor << "." << endl;
+
+	return 0;
+}
