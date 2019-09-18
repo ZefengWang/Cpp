b7ff81937a45e7c75a108eb5dbad952799b32b68
zefengwang <627132520@qq.com>
2019-09-18 20:16:07 +0800

    ch06-ex02

---
+++ ch06/src/ex02.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <array>
+#include <string>
+int main()
+{
+	using namespace std;
+	array < double, 10 > arr;
+	string str;
+	for (int i = 0; i < 10; ++i)
+	{
+		cin >> string;
+		cout << arr[i];
+	}
+	return 0;
+}
